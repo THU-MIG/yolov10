81f5ea9f80bec52d3462fbc217ab5f7cebc693d8
wa22 <wa22@mails.tsinghua.edu.cn>
2024-05-23 06:41:22 +0000

    update

--- ultralytics/models/yolov10/train.py
+++ ultralytics/models/yolov10/train.py
@@ -1,6 +1,8 @@
 from ultralytics.models.yolo.detect import DetectionTrainer
 from .val import YOLOv10DetectionValidator
+from .model import YOLOv10DetectionModel
 from copy import copy
+from ultralytics.utils import RANK
 
 class YOLOv10DetectionTrainer(DetectionTrainer):
     def get_validator(self):
@@ -9,3 +11,10 @@ class YOLOv10DetectionTrainer(DetectionTrainer):
         return YOLOv10DetectionValidator(
             self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
         )
+
+    def get_model(self, cfg=None, weights=None, verbose=True):
+        """Return a YOLO detection model."""
+        model = YOLOv10DetectionModel(cfg, nc=self.data["nc"], verbose=verbose and RANK == -1)
+        if weights:
+            model.load(weights)
+        return model
