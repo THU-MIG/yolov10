ba0e6cbc1a8ed50894abb37a8bd782974826c986
leonnil <liulh23@mails.tsinghua.edu.cn>
2024-06-04 10:29:21 +0000

    fix onnx validation (#179)

--- ultralytics/models/yolov10/val.py
+++ ultralytics/models/yolov10/val.py
@@ -14,7 +14,10 @@ class YOLOv10DetectionValidator(DetectionValidator):
         if isinstance(preds, (list, tuple)):
             preds = preds[0]
         
-        preds = preds.transpose(-1, -2)
-        boxes, scores, labels = ops.v10postprocess(preds, self.args.max_det, self.nc)
-        bboxes = ops.xywh2xyxy(boxes)
-        return torch.cat([bboxes, scores.unsqueeze(-1), labels.unsqueeze(-1)], dim=-1)
+        if preds.shape[-1] == 6:
+            return preds
+        else:
+            preds = preds.transpose(-1, -2)
+            boxes, scores, labels = ops.v10postprocess(preds, self.args.max_det, self.nc)
+            bboxes = ops.xywh2xyxy(boxes)
+            return torch.cat([bboxes, scores.unsqueeze(-1), labels.unsqueeze(-1)], dim=-1)
